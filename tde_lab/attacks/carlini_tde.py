d8cc6c7f3fdb4c96d679b7240367fa9596a3287c
Joonyoung <92843273+mango0727-github@users.noreply.github.com>
2025-09-14 21:50:39 +0900

    Update carlini_tde.py

--- tde_lab/attacks/carlini_tde.py
+++ tde_lab/attacks/carlini_tde.py
@@ -1,1 +1,14 @@
+class CarliniTDE:
+    def __init__(self, model, cfg):
+        self.model = model
+        self.cfg = cfg
 
+    def run(self, hints):
+        outputs = []
+        templates = self.cfg["prompting"]["seed_templates"]
+        for hint in hints:
+            for t in templates:
+                prompt = t.format(hint1=hint, anchor=hint)
+                resp = self.model.generate(prompt, max_tokens=128)
+                outputs.append({"prompt": prompt, "response": resp})
+        return outputs
